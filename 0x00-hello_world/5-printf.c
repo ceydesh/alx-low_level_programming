7f55ee5c24a7a954bf3e2655910f94d01dae2542
ceydesh <hensonouma@gmail.com>
2022-04-10 03:59:03 -0700

    edit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /**
  * main - Entry point
  *
- * return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
