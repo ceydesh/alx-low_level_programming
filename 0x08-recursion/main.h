def8bf9455d63f1e9fb0c3583c78f742a9afbc3d
ceydesh <hensonouma@gmail.com>
2022-04-29 19:47:07 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,9 +10,9 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int actual_sqrt_recursion(int n, int i);
 int is_prime_number(int n);
-int actual_prime(int n, int i)
 int is_palindrome(char *s);
 int get_length(char *s);
+int my_pal(char *s, int l);
 int wildcmp(char *s1, char *s2);
 int findsrc(char *s, char c, int i, int p);
 
